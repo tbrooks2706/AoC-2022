6d891c97934c811e132c4296233723ed8d94daf4
Tom Brooks <tbrooks2706@gmail.com>
2022-12-03 07:22:48 +0000

    start day 3

---
+++ Day_3.py
@@ -0,0 +1,12 @@
+import general_functions
+
+init_list = general_functions.read_file(r"C:\Users\Tom.Brooks\OneDrive - BJSS Ltd\Documents\Coding\AoC-2022\Day_3.txt")
+
+cut_list = init_list[:10]
+print(cut_list)
+
+class Rucksack(string):
+    split = len(string)
+    first_half = string[0]
+
+rucksack1 = Rucksack("fsHtVbjtqstBghhwwPBw")
