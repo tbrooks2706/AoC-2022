2b390fd840c779720cd58041afa1275dd69a7c64
Tom Brooks <tbrooks2706@gmail.com>
2022-12-11 20:35:40 +0000

    Day 10 complete ish

--- Day_10.py
+++ Day_10.py
@@ -1,16 +1,12 @@
 import general_functions
+import math
+
+#part 2 doesn't quite work - displays a grid close but not legible enough. I looked up the answer on Reddit
 
 init_list = general_functions.read_file(r"C:\Users\Tom.Brooks\OneDrive - BJSS Ltd\Documents\Coding\AoC-2022\Day_10.txt")
-#print(init_list)
-
 cut_list = init_list[:10]
-print(cut_list)
-
-#time measured in cycles
-#X is a component of signal strength
-#noop = 1 cycle, does nothing
-#addx num = 2 cycles, increases X by num at the end of the second cycle. nothing happens at the end of the first cycle
-#during the cycle, X is still what it was at the start
+init_grid = general_functions.create_grid(40,6)
+part2_list = general_functions.create_blank_list(".", 240)
 
 class Cycle_Set:
     def __init__(self, num, start_x, instruction):
@@ -44,10 +40,15 @@ class Cycle_Set:
         #increase cycle count by 2 if addx, 1 if noop
         end_list.append(cycle_count)
         return end_list
- 
-example_cycle = Cycle_Set(1, 7, "noop")
-#print(example_cycle.end_x())
-#print(example_cycle.cycle_ends())
+
+class Signal:
+    def __init__(self, cycle_num, x):
+        self.x = int(x)
+        self.cycle_num = int(cycle_num)
+        self.strength = x * cycle_num
+    
+    def __repr__(self):
+        return str(self.cycle_num)
 
 def create_list_of_cycle_ends(input_list):
     #including starting value of X as index 0
@@ -56,25 +57,100 @@ def create_list_of_cycle_ends(input_list):
     for instruction in input_list:
         new_cycles = Cycle_Set(cycle_count, end_list[-1], instruction)
         output_list = new_cycles.cycle_ends()
-        #print("before:",output_list)
         cycle_count = output_list.pop()
-        #print("after:",output_list)
         for item in output_list:
             end_list.append(item)
     return end_list
 list_of_x = create_list_of_cycle_ends(init_list)
-print(list_of_x)
-print(len(list_of_x))
 #during cycle 1, value is index 0
 #value at end of cycle 1 (and during cycle 2), is index 1
 
-#DONE
-#work out what x is at the end of each cycle
-#add it to a list
+def find_signals(cycle_values, cycles_to_check):
+    signals = []
+    for cycle in cycles_to_check:
+        new_signal = Signal(cycle, cycle_values[cycle - 1])
+        signals.append(new_signal.strength)
+    total = sum(signals)
+    return total
 
-#TO DO
-#signal strength at any point = cycle number * X
-    #during cycle 3, X = 15 so signal strength 45
-    #create a signal strength class to calculate this? inputs are cycle num and x value
-#iterate through list_of_x and find value at turns below
-#sum of signal strengths DURING 20, 60, 100, 140, 180, 220
+total_signal_strength = find_signals(list_of_x, [20, 60, 100, 140, 180, 220])
+
+#answer to part 1
+print(total_signal_strength)
+
+#######################PART TWO##########################
+def print_grid(grid):
+    for index in range(len(grid[0])):
+        string = ""
+        for num in range(len(grid)):
+            string += grid[num][index]
+        print(string)
+#print_grid(init_grid)
+#grid is arranged column then row
+    #x then y
+    #cycle 2 is grid[1][0]
+    #cycle 40 is grid[39][0]
+    #cycle 41 is grdi[0][1]
+    #cycle 80 is grid[39][1]
+
+#sprite is 3 pixels wide
+#x = horizontal position of the middle of the sprite
+#CRT draws one pixel per cycle
+    #top row is cycles 1-40, second row 41-80 etc - 240 total
+    #during cycle 1, draw position 0
+#if any of the sprite's 3 pixels cover the dot being drawn in the current cycle, the dot becomes a #
+    #if not, it stays a dot
+#go through all 240 cycle x positions in list_of_x
+    #if cycle_num == x-1, x, or x+1, replace . with #
+    #else do nothing
+#print out grid and it should form 8 letters
+
+def draw_on_list(input_list, cycle_list):
+    index = 0
+    for cycle in range(1, len(cycle_list)):
+        if cycle < 41:
+            target = cycle
+        elif cycle < 81:
+            target = cycle - 40
+        elif cycle < 121:
+            target = cycle - 80
+        elif cycle < 161:
+            target = cycle - 120
+        elif cycle < 201:
+            target = cycle - 160
+        else:
+            target = cycle - 200
+        target -= 1
+        print("cycle",cycle,"index:",index,"target:",target,"x value:",cycle_list[index])
+        if target in [cycle_list[index - 1], cycle_list[index], cycle_list[index + 1]]:
+            input_list[index] = "#"
+        index += 1
+    return input_list
+draw_on_list(part2_list, list_of_x)
+
+def print_list_as_grid(input_list):
+    #shape of grid hard coded for now
+    print("".join(input_list[:40]))
+    print("".join(input_list[40:80]))
+    print("".join(input_list[80:120]))
+    print("".join(input_list[120:160]))
+    print("".join(input_list[160:200]))
+    print("".join(input_list[200:]))
+print_list_as_grid(part2_list)
+
+def draw_on_grid(grid, cycle_list):
+    column = 0
+    row = 0
+    for cycle in range(0, len(cycle_list)):
+        #print("cycle",cycle,"column:",column,"row:",row)
+        if cycle in [cycle_list[column - 1], cycle_list[column], cycle_list[column + 1]]:
+            grid[column][row] = "#"
+        if column == len(grid) - 1:
+            column = 0
+            row += 1
+        else:
+            column += 1
+    return grid
+#new_grid = draw_on_grid(init_grid, list_of_x)
+#print_grid(new_grid)
+#print(len(init_grid))
--- general_functions.py
+++ general_functions.py
@@ -10,3 +10,18 @@ def read_file(file_path):
 
 def reverse_string(string):
     return string[::-1]
+
+def create_grid(width, height):
+    grid = []
+    for num in range(width):
+        column = []
+        for num in range(height):
+            column.append(".")
+        grid.append(column)
+    return grid
+
+def create_blank_list(char, num_of_chars):
+    new_list = []
+    for x in range(num_of_chars):
+        new_list.append(char)
+    return new_list
